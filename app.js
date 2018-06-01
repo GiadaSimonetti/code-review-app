bf6a9fc5e4bc858fb40905236de2d623d6d91cc7
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-01 15:58:46 +0100

    Install body-parser + mongo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 let port = 3000;
-app.listen(port);
-console.log('go to port 3000');
+app.listen(port, () => console.log('listening on 3000'));
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -13,6 +15,10 @@ app.get('/', (req, res) => {
 
 app.get('/new', (req, res) => {
   res.render('new');
+});
+
+app.post('/code-list', (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = app;
